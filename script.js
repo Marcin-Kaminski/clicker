16baef02d74d5208b0eef102cd34402a93462df4
Sz4lejesz <sz3lejesz@gmail.com>
2023-03-23 16:19:57 +0100

    learning  localStorage

--- script.js
+++ script.js
@@ -1,49 +1,87 @@
-var score = 0;
-var power = 1;
-var obsidian = 0;
-var pickCounts = {
-     stonePickCount: 0,
-     ironPickCount: 0,
-     goldPickCount: 0,
-     diamondPickCount: 0,
-};
+let score = parseInt(localStorage.getItem('score')) || 0;
+let power = parseInt(localStorage.getItem('power')) || 1;
+let obsidian = parseInt(localStorage.getItem('obsidian')) || 0;
+let damage = parseInt(localStorage.getItem('damage')) || 0;
+let pickCounts = JSON.parse(localStorage.getItem('picksCount')) || {
+    stonePickCount: 0,
+    ironPickCount: 0,
+    goldPickCount: 0,
+    diamondPickCount: 0,
+}
+let swordCounts = {
+    stoneSwordCount: 0,
+    ironSwordCount: 0,
+    goldSwordCount: 0,
+    diamondSwordCount: 0
+}
+function gEBI(variable) // czemu nie tak? dużo mniej kodu
+{
+    return document.getElementById(variable)
+}
+function addScore()
+{
+    score += power;
+    localStorage.setItem('score', score.toString());
+    gEBI('score').innerHTML = score;
+}
 
 
 window.onload = function() {
     document.getElementById("score").innerHTML = score;
     document.getElementById("power").innerHTML = 'Siła = ' + power;
-    document.getElementById("stonePickaxes").innerHTML = 0;
-    document.getElementById("ironPickaxes").innerHTML = 0;
-    document.getElementById("goldPickaxes").innerHTML = 0;
-    document.getElementById("diamondPickaxes").innerHTML = 0;
-    document.getElementById("obsidian").innerHTML = 0;
-
-}
-function addScore()
-{
-    score = score + power;
-    document.getElementById("score").innerHTML = score;
 }
 function reset()
 {
-    onclick(location.reload())
+    location.reload()
+    power = 0;
+    localStorage.setItem('power', power.toString());
+    score = 0;
+    localStorage.setItem('score', score.toString());
+    document.getElementById("stonePickCount").innerHTML = 0;
+    document.getElementById("ironPickCount").innerHTML = 0;
+    document.getElementById("goldPickCount").innerHTML = 0;
+    document.getElementById("diamondPickCount").innerHTML = 0;
+    document.getElementById("stoneSwordCount").innerHTML = 0;
+    document.getElementById("ironSwordCount").innerHTML = 0;
+    document.getElementById("goldSwordCount").innerHTML = 0;
+    document.getElementById("diamondSwordCount").innerHTML = 0;
+    document.getElementById("enderPearlCount1").innerHTML = 0;
+    document.getElementById("enderPearlCount2").innerHTML = 0;
+    document.getElementById("diamondSwordCount").innerHTML = 0;
+    document.getElementById("obsidian").innerHTML = 0;
 }
 function buyPickaxe(pickaxeCost, plusPower, count)
 {
-    if (score >= pickaxeCost){
+    if (score >= pickaxeCost) {
         score -= pickaxeCost;
         power += plusPower;
         pickCounts[count] += 1;
+        localStorage.setItem('score', score.toString());
+        document.getElementById("score").innerHTML = score;
+        localStorage.setItem('power', power.toString());
+        document.getElementById("power").innerHTML = 'Siła = ' + power;
+        localStorage.setItem(count, count.toString());
+        document.getElementById(count).innerHTML = 'Ilość : ' + pickCounts[count];
+    }
+}
+function buySword(swordCost, plusDamage, count)
+{
+    if (score >= swordCost){
+        score -= swordCost;
+        damage += plusDamage;
+        pickCounts[count] += 1;
         document.getElementById("score").innerHTML = score;
         document.getElementById("power").innerHTML = 'Siła = ' + power;
-        document.getElementById(count).innerHTML = 'Ilość : ' + pickCounts[count];
+        document.getElementById(count).innerHTML = 'Ilość : ' + swordCounts[count];
     }
 }
 function buyObsidian()
 {
     if (score >= 1){
-        score = score + 500000;
-        obsidian = obsidian + 1;
+        score += 500000;
+        localStorage.setItem('score', score.toString());
+        document.getElementById('score').innerHTML = score;
+        obsidian += 1;
         document.getElementById("obsidianCount").innerHTML = "Ilość: " + obsidian;
         obsidian >= 10 ? document.getElementById("portalText").innerHTML = "Kliknij, aby zbudować portal!" : '';
     }
